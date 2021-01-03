8c6cfc828d8195297784b128b44002131b0b9fb6
LNNying <1754102225@qq.com>
2021-01-03 18:31:39 +0800

    初始化vue3.0

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'App',
+        setup() {
+            console.log('adsfsasdf');
+        }
+    }
+</script>
